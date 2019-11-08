9c1b46d8bb824161df14c82b85cfe12cc6ed205d
Evan Altair Ranken <eranken@ur.rochester.edu>
2019-11-08 06:35:07 -0600

    corrected CMSSW version in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 THISDIR=`pwd`
 CMSSW_LOC=$SPACE
-export SCRAM_ARCH=slc6_amd64_gcc530
-cd $CMSSW_LOC/CMSSW_8_1_0
+export SCRAM_ARCH=slc6_amd64_gcc700
+cd $CMSSW_LOC/CMSSW_10_2_18
 eval `scramv1 runtime -sh`
 cd $THISDIR
